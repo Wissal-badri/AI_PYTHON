dda1e2b1495d38522dd56e9bf78cfd92deb3ce0f
Wissal-badri <badriwiwi1@gmail.com>
2026-03-05 10:51:18 +0000

    Refactor code structure for improved readability and maintainability

--- Convertir une image en niveau de gris .py
+++ Convertir une image en niveau de gris .py
@@ -1,8 +1,8 @@
 import cv2
-img = cv2.imread('ressources/image1.jpg')
+img = cv2.imread('Ressources/F1.jpg')
 gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imshow("Image couleur", img)
 cv2.imshow("Image niveaux de gris", gris)
-cv2.imwrite('ressources/image1_gris.jpg',gris)
+cv2.imwrite('Ressources/F1_gris.jpg',gris)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
